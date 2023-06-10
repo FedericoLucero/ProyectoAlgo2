dba670e41f9f63e3a6a7fea2e5df2212bee9c305
PatricioHeduan <heduanpatricio@gmail.com>
2023-06-10 00:33:24 -0300

    create trip and general fixes

--- uber.py
+++ uber.py
@@ -6,6 +6,8 @@ import domain
 grafo = dict() #definimos la variable grafo para la persistencia de los datos
 
 fixUbications = dict()
+
+mobileUbications = dict()
 """ 
 def create_map(local_path):
 Descripcion: Código para crear el mapa utilizando el local_path
@@ -33,6 +35,9 @@ def create_map(local_path):
     uberMap = variety_functions.create_map_dictionary(Vertices,Aristas)
     ubications = dict()
 
+
+    global grafo
+    grafo = uberMap
     variety_functions.open_file_dump("uberMap.pickle", "wb",uberMap)
     variety_functions.open_file_dump("ubications.pickle", "wb",ubications)
     
@@ -44,6 +49,7 @@ def create_map(local_path):
         print("map created successfully")
     else:
         print("ocurrio un error creando el mapa")
+
         
 
 """ 
@@ -80,8 +86,8 @@ def load_fix_element(nombre, direccion):
     fixElement.Address=address
 
     error = variety_functions.load_fix_element(fixElement,grafo,fixUbications,nombre)
-    variety_functions.print_all_nodes(grafo)
     print("loading:",nombre,error)
+    variety_functions.print_all_nodes(grafo)
     variety_functions.print_ubications(fixUbications)
     #Ubicaciones = variety_functions.open_file_load("Ubicaciones.pickle","rb") # Carga el contenido del archivo en la variable "Ubicaciones"
     #diccionary.printHashtable(Ubicaciones)     #eliminar
@@ -117,17 +123,19 @@ def load_movil_element(nombre, direccion, monto):
     element.Address = Address
 
     uberMap = variety_functions.open_file_load("uberMap.pickle","rb") # Carga el contenido del archivo en la variable "uberMap"
-    variety_functions.print_all_nodes(uberMap)
     
     ubications = variety_functions.open_file_load("ubications.pickle","rb") # Carga el contenido del archivo en la variable "ubications"
-    
+    global mobileUbications
+    ubications = mobileUbications
     print(variety_functions.check_element(ubicationName, element, uberMap, ubications))
 
+
     if variety_functions.check_element(ubicationName, element, uberMap, ubications) == None: # Serie de condiciones a cumplir
-        ubications[nombre] = element
+        ubications[nombre] = element   
+        mobileUbications = ubications
         variety_functions.open_file_dump("ubications.pickle","wb",ubications) # Guarda el contenido de la variable "Ubicaciones" en el archivo
         print(f"Se ha cargado un elemento móvil {nombre} en el mapa en la dirección {direccion} con un monto de {monto}")
- 
+
     #diccionary.printHashtable(Ubicaciones)     #eliminar
 
 #def load_movil(mobilElement: domain.mobilUbication, uberMap: dict(),mobilUbications: dict()):
@@ -141,9 +149,35 @@ Herraminetas/Estructuras usadas:
 """
 
 def create_trip(persona, direccion_elemento):
-    #chequear que la persona exista
-    #chequear si ha ingresado una direccion {(ex,int),(ey,int)} o un elemento ej: H1
-    print(f"Se ha creado un viaje para {persona} hacia {direccion_elemento}")
+        global grafo
+        global mobileUbications
+        global fixUbications
+        
+        destiny = direccion_elemento
+        if len(direccion_elemento)>3:
+            destUbi = variety_functions.create_address(direccion_elemento)
+            destiny = destUbi[1][0]
+
+        
+        error= variety_functions.check_person_action(persona,mobileUbications,grafo,direccion_elemento)
+        if error != None:
+            print(error)
+            return
+        
+        nearestCars = variety_functions.find_nearests_3cars(persona,mobileUbications,grafo)
+
+        print(nearestCars)
+        if len(nearestCars)==0:
+            print ("No hay autos disponibles para tu monto actual")
+            return
+        
+        origin = mobileUbications[persona].Address.CornerOrigin.Name
+        print(variety_functions.find_path(grafo,origin,direccion_elemento))
+       
+        variety_functions.move_mobile_elements(mobileUbications,persona,nearestCars[0][0],nearestCars[0][1],destiny,fixUbications)
+
+        variety_functions.print_ubications(mobileUbications)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -161,3 +195,30 @@ if __name__ == "__main__":
         load_movil_element(*args.load_movil_element)
     if args.create_trip:
         create_trip(*args.create_trip)
+    
+    
+    create_map("local_path_original.txt")
+    # Direcciones fijas
+    load_fix_element( "H4", "<e14,3>,<e13,5>")
+    load_fix_element( "H1", "<e2,2>,<e3,2>")
+    load_fix_element( "A1", "<e12,1>,<e16,5>")
+    load_fix_element( "T5", "<e1,10>,<e5,0>")
+    load_fix_element( "S10", "<e9,2>,<e10,2>")
+    
+
+    load_movil_element("P1", "<e10,4>,<e11,4>", "2000")
+    load_movil_element("P2", "<e11,0>,<e7,100>", "4000")
+    load_movil_element("P3", "<e15,2>,<e11,4>", "2500")
+    load_movil_element("P4", "<e4,5>,<e8,5>", "50")
+    load_movil_element("C1", "<e14,2>,<e13,6>", "200")
+    load_movil_element("C2", "<e1,2>,<e2,2>", "50")
+    load_movil_element("C3", "<e3,4>,<e7,4>", "110")
+    load_movil_element("C4", "<e9,1>,<e10,3>", "20")
+    load_movil_element("C5", "<e7,50>,<e11,50>", "25")
+
+    create_trip("P1", "H4")
+    create_trip("P5" ,"<e3,10>,<e2,40>")
+    create_trip("P3" ,"H1")
+    create_trip("P4" ,"<e8,5>,<e4,5>")
+    create_trip("P1" ,"A1")
+    #create_trip ("P2" ,"<e9,50> <e10,0>")
